d2dae391b7bdcf9cb6aebcc57c174c709fc003f9
yillkid <yillkid@gmail.com>
2026-04-13 11:57:47 +0000

    v1.0

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {
-        "version": os.getenv("APP_VERSION", "v1"),
+        "version": os.getenv("APP_VERSION", "v1.0"),
         "status": "ok",
     }
